ab407b62e0044d49503bce5c9126030eae56ab40
Mychef Dev Team <distform.id@gmail.com>
2024-11-08 08:10:44 +0100

    Feat: updated gitignore and context_builder.py.

--- context_builder.py
+++ context_builder.py
@@ -2,6 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 
+# Load enironment
 load_dotenv()
 token = os.getenv("BEARER_TOKEN")
 url = "https://alpha-api.mychef-cloud.com/context"
@@ -17,14 +18,14 @@ try:
     
     with open("COMMON_QUESTIONS.txt", "w", encoding="utf-8") as file:
         for item in data:
-            question = item.get("question", "Pregunta no disponible")
-            answer = item.get("answer", "Respuesta no disponible")
+            question = item.get("question", "Any question avaliable")
+            answer = item.get("answer", "Any answer avaliable")
             
             file.write(f"[PREGUNTA]: {question}\n")
             file.write(f"[RESPUESTA]: {answer}\n")
             file.write("---\n")
     
-    print("Archivo COMMON_QUESTIONS.txt creado con éxito.")
+    print("File COMMON_QUESTIONS.txt created succesfully.")
 
 except requests.exceptions.RequestException as e:
-    print("Ocurrió un error:", e)
+    print("Error:", e)
